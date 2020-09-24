9783aa693e21146e4d2cd7be02dce29fa48b067a
小島和也 <kazuya@kojimakazuyanoMacBook-ea.local>
2020-09-25 01:43:47 +0900

    resources except destroy

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :posts
-  
+  resources :users, except: [:destroy]
+
   get "/" => "home#top"
   get "about" => "home#about"
   get "check" => "home#check"

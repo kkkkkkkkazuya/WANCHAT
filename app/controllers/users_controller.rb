d7491b969963c8788dcf184e7a7647c4dd1deefc
小島和也 <kazuya@kojimakazuyanoMacBook-ea.local>
2020-09-29 23:42:03 +0900

    画像のアップロード

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,12 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(name: params[:name], email: params[:email])
+    @user = User.new(
+      name: params[:name],
+      email: params[:email],
+      image_name: "default.jpg"
+     )
+
     if @user.save
       flash[:notice] = "飼主登録できました！"
       redirect_to(user_path(@user.id))
@@ -29,6 +34,13 @@ class UsersController < ApplicationController
     @user = User.find_by(id: params[:id])
     @user.name = params[:name]
     @user.email = params[:email]
+
+    if params[:image]
+      @user.image_name = "#{@user.id}.jpg"
+      image = params[:image]
+      File.binwrite("public/user_images/#{@user.image_name}", image.read)
+    end
+
     if @user.save
       flash[:notice] = "飼主変更完了！"
       redirect_to(user_path(@user.id))

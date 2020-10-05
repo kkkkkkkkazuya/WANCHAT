da5d985dd112af968c628b474e10441ece72b4b9
小島和也 <kazuya@kojimakazuyanoMacBook-ea.local>
2020-10-05 22:02:27 +0900

    ログイン、新規登録内装変更, /topのURL変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
+  root to: 'home#top'
   resources :posts
   resources :users, except: [:destroy]
 
-  get "/" => "home#top"
+  get "top" => "home#top"
   get "about" => "home#about"
   get "check" => "home#check"
 end

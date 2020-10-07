b208b2959f651045a96e3531d673b4998be25889
小島和也 <kazuya@kojimakazuyanoMacBook-ea.local>
2020-10-07 22:32:39 +0900

    ログイン、新規飼主日本語化

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'home#top'
   resources :posts
   resources :users, except: [:destroy]
 
-  get "top" => "home#top"
+  get "/" => "home#top"
   get "about" => "home#about"
   get "check" => "home#check"
 end

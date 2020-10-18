9831a00a026622487cf77cf0a21394b8f646fda3
小島和也 <kazuya@kojimakazuyanoMacBook-ea.local>
2020-10-18 17:53:03 +0900

    seedデータの追加、userとpostsの紐付け

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,10 +2,4 @@ user1 = User.create!(name: "和也", email: "kazuya@hoge", password: "password")
 user2 = User.create!(name: "ゆうき", email: "yuhki@hoge", password: "password")
 user3 = User.create!(name: "かな", email: "kana@hoge", password: "password")
 
-user1.posts.create!(content: "ミニチュアダックス可愛い")
-user2.posts.create!(content: "どんな犬がおすすめですか？")
-user3.posts.create!(content: "大型犬がおすすめですよ！")
-user3.posts.create!(content: "ゴールデンレトリバー賢いですよ！")
-user1.posts.create!(content: "柴犬もいいと思います！")
-
 puts "初期データ転送完了しました。"

169f66fea36ab5f18fff5008687090a3051b3a1a
小島和也 <kazuya@kojimakazuyanoMacBook-ea.local>
2020-10-18 17:38:42 +0900

    posts/indexのデザイン変更、seedデータの追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+user1 = User.create!(name: "和也", email: "kazuya@hoge", password: "password")
+user2 = User.create!(name: "ゆうき", email: "yuhki@hoge", password: "password")
+user3 = User.create!(name: "かな", email: "kana@hoge", password: "password")
+
+user1.posts.create!(content: "ミニチュアダックス可愛い")
+user2.posts.create!(content: "どんな犬がおすすめですか？")
+user3.posts.create!(content: "大型犬がおすすめですよ！")
+user3.posts.create!(content: "ゴールデンレトリバー賢いですよ！")
+user1.posts.create!(content: "柴犬もいいと思います！")
+
+puts "初期データ転送完了しました。"
